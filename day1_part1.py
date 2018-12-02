7ff0f02a82b45369cd86c6a4ffa43ecac861298b
Mike Yu <myu@whitepages.com>
2018-12-01 19:14:49 -0800

    Dec 1

---
+++ day1_part1.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3
+# -*- coding: UTF-8 -*-
+"""https://adventofcode.com/2018/day/1
+
+--- Day 1: Chronal Calibration (part 1)---
+
+After feeling like you've been falling for a few minutes, you look at the
+device's tiny screen. "Error: Device must be calibrated before first use.
+Frequency drift detected. Cannot maintain destination lock." Below the message,
+the device shows a sequence of changes in frequency (your puzzle input). A value
+like +6 means the current frequency increases by 6; a value like -3 means the
+current frequency decreases by 3.
+
+For example, if the device displays frequency changes of +1, -2, +3, +1, then
+starting from a frequency of zero, the following changes would occur:
+
+Current frequency  0, change of +1; resulting frequency  1.
+Current frequency  1, change of -2; resulting frequency -1.
+Current frequency -1, change of +3; resulting frequency  2.
+Current frequency  2, change of +1; resulting frequency  3.
+In this example, the resulting frequency is 3.
+
+Here are other example situations:
+
++1, +1, +1 results in  3
++1, +1, -2 results in  0
+-1, -2, -3 results in -6
+
+Starting with a frequency of zero, what is the resulting frequency after all of
+the changes in frequency have been applied?
+"""
+import pyperclip
+
+from puzzle_inputs import day1_inputs
+
+# Note: puzzle inputs differ by user
+ans = sum([int(i) for i in day1_inputs])
+
+print(f'The answer is: {ans:,}')
+pyperclip.copy(ans)
